f1750d692d2ad9ea38001ffd7fa74b738b3480ad
BLJett <65201151+BLJett@users.noreply.github.com>
2020-06-29 20:24:45 -0500

    Added existing files to the repository

---
+++ Rock Paper Scissors/Rock Paper Scissors/Main.cpp
@@ -0,0 +1,125 @@
+// This is a first attempt at a Rock Paper Scissors game
+// This will also be the first project uploaded to Github
+//
+// Brandon Jett
+// Start Date: 5/11/20
+
+#include <iostream>
+#include <string>
+#include <ctype.h>
+using namespace std;
+
+bool determineOpponent();
+int getRPS(); 
+
+int main()
+{
+	cout << "Welcome to Rock Paper Scissors" << endl << endl;
+	//determineOpponent();
+	
+	//Get the Rock Paper Scissors of user 1 and cpu,
+	//if playing vs another user get input of user 2
+	//as well
+	if (determineOpponent() == true) //User playing against CPU
+	{
+		cout << "User 1 ";
+		int user_input = getRPS();
+
+		//insert function call for CPU to determine its hand here
+	}
+	else
+	{
+		cout << "User 1 ";
+		int user1_input = getRPS();
+
+		cout << "User 2 ";
+		int user2_input = getRPS();
+	}
+	
+
+	system("pause");
+	return 0;
+	
+}
+
+//********************************************************
+//This function will determine if the player wants to play
+//against a computer (RNG) or play against another human
+//
+//Params: none
+//return: bool
+//********************************************************
+bool determineOpponent()
+{
+	char userResponse = ' ';	//User response to determine if playing cpu
+	bool playComputer;			//returned value, if true, CPU, else human
+
+	cout << "Do you want to play against a Computer? (Y/N): ";
+	cin >> userResponse;
+	cin.clear();
+	cin.ignore(numeric_limits <streamsize> ::max(), '\n');	//not sure exactly as to how to use cin.ignore but this works
+	userResponse = char(tolower(userResponse)); 
+
+	if (userResponse != 'y'  && userResponse != 'n')
+	{
+		cout << "Response not valid, please enter only \"Y\" or \"N\"." << endl << endl << endl;
+		determineOpponent();
+	}
+		
+
+	if (userResponse == 'n')
+	{
+		playComputer = false;
+		cout << "You have chosen to play against another human." << endl;
+	}
+	else
+	{
+		playComputer = true;
+		cout << "You have chosen to play against the CPU." << endl;
+	}
+		
+	return playComputer;
+}
+
+//********************************************************
+//This function will determine from the user which hand 
+//sign they want to use
+//
+//Params: none
+//return: int
+//********************************************************
+int getRPS()
+{
+	int handsign = 1;
+
+
+	cout << "Pick your move: " << endl << endl;
+	cout << "Enter 1 for Rock '\n' Enter 2 for Scissors '\n' Enter 3 for Paper: ";
+	cin >> handsign;
+	cin.clear();
+	cin.ignore(numeric_limits <streamsize> ::max(), '\n');
+	
+	//isdigit is always returning false, but somehow the code works so
+	if (isalpha(handsign) || handsign < 1 || handsign > 3)
+	{
+		cout << "Invalid input: Please only enter 1, 2, or 3." << endl;
+		getRPS();
+	}
+	else if (handsign == 1) //User chose Rock
+	{
+		cout << "User 1 selected Rock." << endl;
+		return 1;
+	}
+	else if (handsign == 2) //User chose Scissors
+	{
+		cout << "User 1 selected Scissors." << endl;
+		return 2;
+	}
+	else if (handsign == 3) //User chose Paper
+	{
+		cout << "User 1 selected Paper." << endl;
+		return 3;
+	}
+
+	return handsign;
+}
